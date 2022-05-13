d2748b109f8bddb66ddd6f33e95cfa0e1a9116f5
Georgiy <dev.georgiy@gmail.com>
2022-05-13 20:20:09 +0300

    Правки в cpu_info

--- cpu_usage.sh
+++ cpu_usage.sh
@@ -8,7 +8,7 @@ fi
 
 function send_cpu() {
   cpu="$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]"
-  echo "cpu_usage $cpu" | curl --data-binary @-  http://localhost:9091/metrics/job/cpu_usage/instance/$1
+  echo "cpu_usage $cpu" | curl --data-binary @-  http://localhost:9091/metrics/job/cpu_usage/instance/$1 > /dev/null
 }
 
 while sleep 1; do
